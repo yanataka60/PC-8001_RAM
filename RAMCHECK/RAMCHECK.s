90a45ff56dafbe94ae6e7fae7f4febdbf9489034
yanataka60 <92910500+yanataka60@users.noreply.github.com>
2025-08-31 13:53:47 +0900

    Add files via upload

--- RAMCHECK/RAMCHECK.s
+++ RAMCHECK/RAMCHECK.s
@@ -6,37 +6,37 @@ MSGOUT		EQU		52EDH		;文字列の出力2
 
 			ORG		9000H
 
-			LD		HL,0000H
-			LD		DE,8000H
+			LD		HL,0000H	;CHECK開始アドレス
+			LD		DE,8000H	;CHECK終了アドレス+1
 			
-LOP1:		CALL	MONCLF
-			CALL	MONDHL
-			CALL	MONSPC
+LOP1:		CALL	MONCLF		;改行
+			CALL	MONDHL		;CHECKアドレス表示
+			CALL	MONSPC		;空白
 
-			LD		A,55H
-			CALL	PRINTA
-			CALL	CHGRAM
-			LD		(HL),A
-			LD		A,(HL)
-			CALL	CHGROM
-			CALL	PRINTA
-			CP		55H
-			JR		NZ,ERR
+			LD		A,55H		;書込DATA
+			CALL	PRINTA		;書込DATA表示
+			CALL	CHGRAM		;RAMへ切り替え
+			LD		(HL),A		;CHECK DATA 書込
+			LD		A,(HL)		;CHECK DATA 読込
+			CALL	CHGROM		;ROMへ切り替え
+			CALL	PRINTA		;読込DATA表示
+			CP		55H			;読込DATA比較
+			JR		NZ,ERR		;一致しなければERR
 			
-			LD		A,0AAH
-			CALL	PRINTA
-			CALL	CHGRAM
-			LD		(HL),A
-			LD		A,(HL)
-			CALL	CHGROM
-			CALL	PRINTA
-			CP		0AAH
-			JR		NZ,ERR
+			LD		A,0AAH		;書込DATA
+			CALL	PRINTA		;書込DATA表示
+			CALL	CHGRAM		;RAMへ切り替え
+			LD		(HL),A		;CHECK DATA 書込
+			LD		A,(HL)		;CHECK DATA 読込
+			CALL	CHGROM		;ROMへ切り替え
+			CALL	PRINTA		;読込DATA表示
+			CP		0AAH  		;読込DATA比較
+			JR		NZ,ERR		;一致しなければERR
 			
-			INC		HL
+			INC		HL			;CHECK開始アドレス+1
 			PUSH	HL
 			AND		A
-			SBC		HL,DE
+			SBC		HL,DE		;CHECK開始アドレスがCHECK終了アドレス以下ならループ
 			POP		HL
 			JR		NZ,LOP1
 			LD		HL,MSG_OK
@@ -50,21 +50,21 @@ MSG_OK:		DB		'RAM OK!',0DH,0AH,00H
 
 MSG_ERR:	DB		'RAM ERROR!',0DH,0AH,00H
 
-CHGRAM:
+CHGRAM:							;RAMへ切り替え
 			PUSH	AF
 			LD		A,11H
 			OUT		(0E2H),A
 			POP		AF
 			RET
 
-CHGROM:
+CHGROM:							;ROMへ切り替え
 			PUSH	AF
 			XOR		A
 			OUT		(0E2H),A
 			POP		AF
 			RET
 
-PRINTA:
+PRINTA:							;Aレジスタの内容を16進数2桁で表示
 			PUSH	AF
 			PUSH	HL
 			LD		HL,WORKA
